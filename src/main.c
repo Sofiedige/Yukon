466de7c6301d7f68c74b32129647c6cf7055568b
Sebastian Ellegaard <sebynoah@gmail.com>
2022-04-22 11:12:43 +0200

    Addfirst, addCard, og splitning af createtempdeck og LD

--- src/main.c
+++ src/main.c
@@ -1,36 +1,74 @@
 #include <stdio.h>
 #include <string.h>
 typedef struct String String;
+void *malloc(size_t size);
 
-typedef struct card{
+
+typedef struct node{
     char suit;
-    int rank;
-    struct card* next;
-    struct card* prev;
-};
+    char rank;
+    struct node* next;
+}node;
+node* head = NULL;
 
+struct node *addFirst(char rank, char suit){
+    node *newNode = malloc(sizeof (node));
+    newNode->rank=rank;
+    newNode->suit=suit;
+    newNode->next=NULL;
+    head = newNode;
+    return newNode;
+}
 
-void LD(char file[]){
-    char tempDeck[104];
+struct node *addCard(char rank, char suit){
+    node *newNode = malloc(sizeof (node));
+    newNode->rank=rank;
+    newNode->suit=suit;
+    newNode->next=NULL;
+    return newNode;
+}
+
+void print(node *head) {
+    node *currentNode = head;
+    while (currentNode != NULL) {
+
+        printf("%c%c ", currentNode->rank,currentNode->suit);
+
+    }
+}
+char tempDeck[104];
+
+void createTempDeck(char file[]){
     FILE *in = fopen(file,"r");
     if(!in){
         printf("fejl\n");
     }
     int wordCount;
     fscanf(in,"%d ",&wordCount);
-
     for (int i = 0; i < wordCount; ++i) {
         fscanf(in,"%c\n",&tempDeck[i]);
-        printf("%c\n",tempDeck[i]);
     }
-
-
-    for (int i = 0; i < wordCount/2; ++i) {
-
-
-    }
-
 }
+
+
+void LD(char file[]){
+    createTempDeck(file);
+    for (int i = 0; i < 104; ++i) {
+        if (!head) {
+            addFirst(tempDeck[i], tempDeck[i + 1]);
+        }
+        if (head) {
+        addCard(tempDeck[i], tempDeck[i + 1]);
+        }
+        i++;
+    }
+}
+
+
+
+
+
+
 
 int Sl(){
 }
