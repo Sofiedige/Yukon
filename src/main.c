b0abdb01b4f759958ceca94dcf8e1523d075c7a1
MathildeElia <91071893+MathildeElia@users.noreply.github.com>
2022-04-22 08:45:11 +0200

    terminal start.

--- src/main.c
+++ src/main.c
@@ -1,10 +1,16 @@
+
 typedef struct String String;
+#include <stdio.h>
 
-void main(){
+void main() {
+    printf("C1  C2  C3  C4  C5  C6  C7\n");
+    printf("                                        F1\n");
+    printf("                                        F2\n");
+    printf("                                        F3\n");
+    printf("                                        F4\n");
 
+}
 
-
-};
 
 
 int Card(){
