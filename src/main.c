a6f5d822041e333cd9dee190db604fb8a1ae4223
MathildeElia <91071893+MathildeElia@users.noreply.github.com>
2022-04-27 23:11:05 +0200

    Print method adjustet.

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@ typedef struct node{
 }node;
 
 struct node* head = NULL;
-struct node* prevNode=NULL;
+struct node* prevNode = NULL;
 
 struct node *addFirst(char suit, char rank){
     node *newNode = malloc(sizeof (node));
@@ -25,96 +25,20 @@ struct node *addFirst(char suit, char rank){
 
 struct node *addCard(char suit, char rank){
     node *newNode = malloc(sizeof (node));
+
+    //sætter forrige nodes next til newNode
+    //prevNode = head;
+    prevNode->next=newNode;
+
     newNode->rank=rank;
     newNode->suit=suit;
     newNode->next=NULL;
     newNode->prev=prevNode;
+
+    prevNode = newNode;
+
     return newNode;
 }
-
-void print(node *head) {
-    struct node *arr[7];
-    int j=1;
-    node *currentNode = head->next;
-
-    for (int i = 0; i < 7; ++i) {
-        if(i==0){
-            arr[i]=head;
-            arr[i]->next=NULL;
-            arr[i]->prev=NULL;
-            currentNode=head->next;
-        }
-        if(i>0){
-            arr[i]=currentNode;
-            arr[i]->next=NULL;
-            arr[i]->prev=NULL;
-            currentNode = currentNode->next;
-        }
-
-    }
-
-    for (int i = 0; i < 45; ++i) {
-        if(i<=30) {
-            if(j>6) {
-                j=1;
-            }
-            arr[j]=currentNode;
-            arr[j]->prev=arr[j];
-            arr[j]->next=NULL;
-            j++;
-        }
-
-        if(i>30&&i<=35) {
-            if(j>5) {
-                j=1;
-            }
-            arr[j+1]->prev=arr[j+1];
-            arr[j+1]=currentNode;
-            arr[j+1]->next=NULL;
-            j++;
-        }
-
-        if(i>35&&i<=39) {
-            if(j>4) {
-                j=1;
-            }
-            arr[j+2]->prev=arr[j+2];
-            arr[j+2]=currentNode;
-            arr[j+2]->next=NULL;
-            j++;
-        }
-
-        if(i>39&&i<=42) {
-            if(j>3) {
-                j=1;
-            }
-            arr[j+3]->prev=arr[j+3];
-            arr[j+3]=currentNode;
-            arr[j+3]->next=NULL;
-            j++;
-        }
-        if(i>42&&i<=44) {
-            if(j>2) {
-                j=1;
-            }
-            arr[j+4]->prev=arr[j+4];
-            arr[j+4]=currentNode;
-            arr[j+4]->next=NULL;
-            j++;
-        }
-
-        if(i>44) {
-            if(j>1) {
-                j=1;
-            }
-            arr[j+5]->prev=arr[j+4];
-            arr[j+5]=currentNode;
-            arr[j+5]->next=NULL;
-            j++;
-        }
-    }
-    }
-
 
 char tempDeck[104];
 
@@ -125,23 +49,43 @@ void createTempDeck(char file[]){
     }
     int wordCount;
     fscanf(in,"%d ",&wordCount);
+
     for (int i = 0; i < wordCount; ++i) {
         fscanf(in,"%c\n",&tempDeck[i]);
+    }
+
+    for (int j = 0; j < 104; ++j) {
+        if (head == NULL) {
+            addFirst(tempDeck[j], tempDeck[j + 1]);
+        }
+        if (head != NULL) {
+            addCard(tempDeck[j], tempDeck[j + 1]);
+        }
+        j++;
+    }
+}
+
+void print(node *head) {
+    int count = 0;
+    node *current_node = head;
+    while ( current_node != NULL) {
+        count++;
+        if(count > 7) {
+            printf("\n%c%c ", current_node->suit, current_node->rank);
+            current_node = current_node->next;
+            count=1;
+        }
+        else{
+            printf("%c%c ", current_node->suit, current_node->rank);
+            current_node = current_node->next;
+        }
     }
 }
 
 
 void SW(char file[]) {
     createTempDeck(file);
-    for (int i = 0; i < 104; ++i) {
-        if (head == NULL) {
-            addFirst(tempDeck[i], tempDeck[i + 1]);
-        }
-        if (head != NULL) {
-            addCard(tempDeck[i], tempDeck[i + 1]);
-        }
-        i++;
-    }
+
     struct node *arr[7];
     int j=1;
     for (int i = 0; i < 52; ++i) {
@@ -153,8 +97,6 @@ void SW(char file[]) {
     printf("%s\n", addCard(tempDeck[2],tempDeck[3]));
 
 }
-
-
 
     void LD(char file[]){
         node *current=head->next;
@@ -173,10 +115,6 @@ void SW(char file[]) {
                 printf("%d%d\n",current->rank,current->suit);
                 current=current->next;
         }
-
-
-
-
 }
 
 int Sl(){
@@ -184,16 +122,8 @@ int Sl(){
 
 
 int main(){
-    SW("Deck");
+    createTempDeck("Deck");
+    print(head);
+    //SW("Deck");
 }
-
-
-
-
-
-
-
-
-
-
 
